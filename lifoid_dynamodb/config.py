6d1fc26f3c17cf32b3dda48c8d625eef7b2faa7d
Lifoid <romarydyv@gmail.com>
2020-06-14 10:45:19 +0200

    Init config

--- lifoid_dynamodb/config.py
+++ lifoid_dynamodb/config.py
@@ -8,8 +8,5 @@ from lifoid.config import Configuration, environ_setting
 
 class DynamoDBConfiguration(Configuration):
     """
-    Rasa_nlu configuration
+    Dynamodb configuration
     """
-    host = environ_setting('REDIS_HOST', '127.0.0.1', required=False)
-    port = environ_setting('REDIS_PORT', 6379, required=False)
-    db = environ_setting('REDIS_DB', 0, required=False)
